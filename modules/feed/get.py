0a59d503377f8abe5bbe60934afc7988fb69aa7e
Akbar <akbarjonjorayev001@gmail.com>
2025-01-25 19:41:39 +0500

    get feed by uid

--- modules/feed/get.py
+++ modules/feed/get.py
@@ -1,8 +1,8 @@
 import validator
 
 
-def get(user_id: int, feed_id: int, supabase) -> dict:
-    is_valid_get_input = validator.valid_get_input(user_id, feed_id)
+def get(user_id: int, feed_uid: str, supabase) -> dict:
+    is_valid_get_input = validator.valid_get_input(user_id, feed_uid)
     if not is_valid_get_input["ok"]:
         return is_valid_get_input
 
@@ -11,7 +11,7 @@ def get(user_id: int, feed_id: int, supabase) -> dict:
             supabase.table("feeds")
             .select("title, text, created_at, updated_at, uid")
             .eq("user_id", user_id)
-            .eq("feed_id", feed_id)
+            .eq("uid", feed_uid)
             .execute()
         )
         if not response.data:
@@ -39,4 +39,4 @@ def get_all(user_id: int, supabase) -> dict:
     except Exception as e:
         return {"ok": False, "message": str(e)}
 
-    return {"ok": True, "message": "Feed gotten", "data": response.data}
+    return {"ok": True, "message": "Feeds gotten", "data": response.data}
